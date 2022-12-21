82b72cf58d0bd4fa4853d8921f30e90ef1a9804d
aliza614 <47804976+aliza614@users.noreply.github.com>
2022-12-21 15:09:00 -0600

    this is the solution

--- src/app/exercise/arbiter.service.ts
+++ src/app/exercise/arbiter.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, ReplaySubject } from 'rxjs';
+import { Observable, Subject, ReplaySubject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArbiterService {
   public item$: Observable<string>;
-  private itemSource: Subject<string> = new ReplaySubject(1);
+  private itemSource: Subject<string> = new BehaviorSubject('Initial');
   constructor() {
     this.item$ = this.itemSource.asObservable();
   }
 
   public emit(item: string): void {
     this.itemSource.next(item);
+  }
+
+  public getCurrent(): string {
+    return (this.itemSource as BehaviorSubject<string>).value;
   }
 }
